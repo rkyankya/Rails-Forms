fafb194bb406b23a16f4152888b64ab728adb595
Kyankya Raymond <rkyankya@rkyankya.rkyankya>
2020-01-23 13:20:23 +0300

    Added Bootstrap

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'faker'
 gem 'better_errors'
 gem 'hirb'
+gem 'bootstrap', '~> 4.4.1'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,16 +1,28 @@
 class DriversController < ApplicationController
   def index
+    @driver = Driver.all
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
+    @driver = Driver.create(driver_params)
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+  def set_driver
+    @driver = Driver.find(params[:id])
+  end
+
+  def driver_params
+    params.require(:driver).permit(:name, :address)
   end
 end
